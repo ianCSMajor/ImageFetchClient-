da8aa875ff8e2dfd5d2055f4b9802c49570b2210
Ian Oh <IOh@oceaneering.net>
2022-09-16 15:39:34 -0400

    Changed LoadMethod name to LaodMethodAsync

--- Program.cs
+++ Program.cs
@@ -103,7 +103,7 @@ public class fetchImageFromRestfulApi
             fetchPicWithCaption = fetchPicWithoutCaption + @"/says/" + PictureCaption;
             url = fetchPicWithCaption;
         }
-        await DownloadPicture(url, sendToPath);
+        await DownloadPictureAsync(url, sendToPath);
         
     }
     /// <summary>
